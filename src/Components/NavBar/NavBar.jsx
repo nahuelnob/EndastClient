611e11b17a20c220e4e1bda3850181b35bce98b5
Nahue <nahue.santini@gmail.com>
2024-01-15 10:41:36 -0300

    Cambios en la Navbar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,13 +1,12 @@
 import style from "./NavBar.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 const NavBar = () => {
+  const location = useLocation()
+  console.log(location.pathname);
   const placas = useSelector((state) => state.placas);
   return (
     <nav className={style.container}>
-      {/* <header className={style.imagen}>
-        
-      </header> */}
       <section className={style.logo}>
         <img className={style.iso} src="../../public/iso.png" alt="" />
         <p className={style.padi}>Padi</p>
@@ -21,15 +20,15 @@ const NavBar = () => {
               style={{ textDecoration: "none", color: "white" }}
             >
               <div className={style.placas}>
-                <button className={style.link}>{dId}</button>
+               {location.pathname === `/${dId}` ? <button className={style.linkActive}>{dId}</button> : <button className={style.link}>{dId}</button>}
               </div>
             </NavLink>
           );
         })}
       </section>
-<footer className={style.footer}>
-<p>v3.0.1</p>
-</footer>
+      <footer className={style.footer}>
+        <p>v3.0.1</p>
+      </footer>
     </nav>
   );
 };
