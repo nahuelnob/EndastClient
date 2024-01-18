472cae621dadb9ab9c7fe4af058d9b3fb36cb1a0
Nahue <nahue.santini@gmail.com>
2024-01-18 20:34:30 -0300

    debbug login

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -24,15 +24,15 @@ const Landing = () => {
   const handlerAccess = (user, pass) => {
     user !== usuario && window.alert("Usuario incorrecto");
     pass !== password && window.alert("Contraseña incorrecta");
-    user === usuario &&
-      pass === password &&
-      dispatch(addPlacas()) &&
+    if (user === usuario && pass === password) {
+      dispatch(addPlacas())
       setAccess(true);
+    }
   };
 
   return (
     <div className={style.container}>
-      <main className={style.brand}/>
+      <main className={style.brand} />
 
       <main className={style.main}>
         <header className={style.logo}>
@@ -44,34 +44,34 @@ const Landing = () => {
 
         <form className={style.form}>
           {/* <section className={style.section}> */}
-            <p className={style.etiquetaUs}>Usuario</p>
-            <input
-              className={style.input}
-              type="text"
-              autocomplete="off"
-              name="usuario"
-              value={userData.usuario}
-              onChange={handleChange}
-            />
+          <p className={style.etiquetaUs}>Usuario</p>
+          <input
+            className={style.input}
+            type="text"
+            autocomplete="off"
+            name="usuario"
+            value={userData.usuario}
+            onChange={handleChange}
+          />
           {/* </section> */}
           {/* <section className={style.section}> */}
-            <p className={style.etiquetaCon}>Contraseña</p>
-            <input
-              className={style.input}
-              type="password"
-              autocomplete="off"
-              name="password"
-              value={userData.password}
-              onChange={handleChange}
-            />
+          <p className={style.etiquetaCon}>Contraseña</p>
+          <input
+            className={style.input}
+            type="password"
+            autocomplete="off"
+            name="password"
+            value={userData.password}
+            onChange={handleChange}
+          />
           {/* </section> */}
           {/* <section className={style.section}> */}
-            <button
-              className={style.boton}
-              onClick={() => handlerAccess(userData.usuario, userData.password)}
-            >
-              Entrar
-            </button>
+          <button
+            className={style.boton}
+            onClick={() => handlerAccess(userData.usuario, userData.password)}
+          >
+            Entrar
+          </button>
           {/* </section> */}
         </form>
       </main>
