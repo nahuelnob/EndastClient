6c73150f9dfbe5a64c054a86d8f467b925fc7ac5
Nahue <nahue.santini@gmail.com>
2024-01-24 18:18:24 -0300

    actualización dins

--- src/Components/Din3/Din3.jsx
+++ src/Components/Din3/Din3.jsx
@@ -3,8 +3,6 @@ import { editDin3 } from "../../Redux/actions";
 import React, { useEffect, useState } from "react";
 import mqtt from "mqtt";
 import style from "./Din3.module.css";
-// const dId = "testid";
-// const TOPIC = `64c314be56857449102a9d4b/${dId}/9UaEQR4I36/sdata`;
 const HOST = "192.168.0.46";
 
 const Din3 = ({ topic }) => {
@@ -54,7 +52,7 @@ const Din3 = ({ topic }) => {
         </button>
       </header>
       <main className={style.main}>
-      {din3 ?
+      {din3[dId] ?
          <section className={style.encendido}>
          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{ width: "4vh", filter:"saturate(350%)", filter:"drop-shadow(0px 0px 3px #63dcec)"}}><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V256c0 17.7 14.3 32 32 32s32-14.3 32-32V32zM143.5 120.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6c-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4c0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z" fill="#63dcec" /></svg><span style={{position:"absolute", left:"5vh"}}>On</span>
        </section> :
